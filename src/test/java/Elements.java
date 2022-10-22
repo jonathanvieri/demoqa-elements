598816f17c00ab5960e6a9d51cb56c52dd647b4f
Digionix <jonathanvieri1321@gmail.com>
2022-10-22 21:09:42 +0700

    Add Web Tables test

--- src/test/java/Elements.java
+++ src/test/java/Elements.java
@@ -77,6 +77,8 @@ public class Elements {
     @Test(description = "Go to Radio Button section and press the buttons")
     public void step4() {
         driver.findElement(By.xpath("//li//span[text()='Radio Button']")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='main-header' and text()='Radio Button']")).isDisplayed(),
+                "Radio Button header not found / not in Radio Button page");
 
         // Click buttons and verify the value
         JavascriptExecutor executor = (JavascriptExecutor) driver;
@@ -90,6 +92,45 @@ public class Elements {
         executor.executeScript("arguments[0].click();", element);
         Assert.assertTrue(driver.findElement(By.xpath("//span[@class='text-success' and text()='Impressive']")).isDisplayed(),
                 "Impressive is selected but text not found");
+    }
+
+    @Test(description = "Go to Web Tables section and interact with the table")
+    public void step5() {
+        driver.findElement(By.xpath("//li//span[text()='Web Tables']")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='main-header' and text()='Web Tables']")).isDisplayed(),
+                "Web Tables header not found / not in Web Tables page");
+
+        // New entry variables
+        String firstName = "Roronoa";
+        String lastName = "Zoro";
+        String email = "zoro@example.com";
+        String age = "21";
+        String salary = "1111";
+        String department = "Swordsman";
+
+        // Add a new entry
+        driver.findElement(By.xpath("//button[@Id='addNewRecordButton']")).click();
+        driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys(firstName);
+        driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys(lastName);
+        driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys(email);
+        driver.findElement(By.xpath("//input[@id='age']")).sendKeys(age);
+        driver.findElement(By.xpath("//input[@id='salary']")).sendKeys(salary);
+        driver.findElement(By.xpath("//input[@id='department']")).sendKeys(department);
+        driver.findElement(By.xpath("//button[@id='submit']")).click();
+
+        // Verify the entry is available
+        Assert.assertTrue(driver.findElement(By.xpath("//div[contains(text(), '" + firstName + "')]")).isDisplayed(),
+                "First name not found");
+        Assert.assertTrue(driver.findElement(By.xpath("//div[contains(text(), '" + lastName + "')]")).isDisplayed(),
+                "Last name not found");
+        Assert.assertTrue(driver.findElement(By.xpath("//div[contains(text(), '" + email + "')]")).isDisplayed(),
+                "Email not found");
+        Assert.assertTrue(driver.findElement(By.xpath("//div[contains(text(), '" + age + "')]")).isDisplayed(),
+                "Age not found");
+        Assert.assertTrue(driver.findElement(By.xpath("//div[contains(text(), '" + salary + "')]")).isDisplayed(),
+                "Salary not found");
+        Assert.assertTrue(driver.findElement(By.xpath("//div[contains(text(), '" + department + "')]")).isDisplayed(),
+                "Department not found");
     }
 
     @AfterTest(description = "Finished the test, cleaning up ChromeDriver instance")
